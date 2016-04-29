a00f92567bf19081f434e0bfbdc3e63af107e7e6
Spiros Delviniotis <spirosdelviniotis@gmail.com>
2016-04-29 15:20:11 +0300

    Initial commit of Project: parallel_programming_mpi

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: SpirosDelvis
+ *
+ * Created on 29 Απριλίου 2016, 3:18 μμ
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
